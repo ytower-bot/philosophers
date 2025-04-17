4a8c095b06d0710dd5d6b6014623e1f143be7ea5
ytower <ytower@proton.me>
2025-04-17 14:46:54 -0300

    feat: ft_strlen && exit_failure

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,12 +6,21 @@
 /*   By: yfaustin <yfaustin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 12:56:07 by yfaustin          #+#    #+#             */
-/*   Updated: 2025/04/17 14:05:18 by yfaustin         ###   ########.fr       */
+/*   Updated: 2025/04/17 14:40:58 by yfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
 
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+// utils
+int	ft_strlen(char *str);
+
+// error
+void	exit_error(char *message);
 
 #endif
