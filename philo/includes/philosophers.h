3ec465e929afeaccaae8cbe562c777a68acbc836
yfaustin <yfaustin@student.42.rio>
2025-06-03 14:59:24 -0300

    feat: utils

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: yfaustin <yfaustin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 12:56:07 by yfaustin          #+#    #+#             */
-/*   Updated: 2025/06/03 12:57:09 by yfaustin         ###   ########.fr       */
+/*   Updated: 2025/06/03 14:58:17 by yfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,20 @@ typedef struct s_args
 	long long	time_to_die;
 	long long	time_to_eat;
 	long long	time_to_sleep;
-	int			n_of_rouds;
+	int			n_of_rounds;
 	int			valid_args;
 }				t_args;
 
 // utils
-int	ft_strlen(char *str);
+int		ft_isdigit(int c);
+int		ft_atopi(const char *str, int *res);
+int		ft_atopll(const char *str, long long *res);
+void	ft_putstr_fd(const char *str, int fd);
+
+// check args
 int	check_args(t_args *args, int argc, char **argv);
 
 // error
-void	exit_error(char *message);
+int	print_error(char *message);
 
 #endif
