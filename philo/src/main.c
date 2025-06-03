817d6ccb283f6fc700856a1be33d9534cf860a81
yfaustin <yfaustin@student.42.rio>
2025-06-03 15:16:31 -0300

    parser complete

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: yfaustin <yfaustin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 14:06:01 by yfaustin          #+#    #+#             */
-/*   Updated: 2025/06/02 19:33:19 by yfaustin         ###   ########.fr       */
+/*   Updated: 2025/06/03 15:08:36 by yfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,12 @@
 
 int	main(int argc, char **argv)
 {
-	t_args	*args;
+	t_args	args;
 
-	check_args(args, argc, argv);
+	if (!(check_args(&args, argc, argv)))
+		return (1);
+	printf("got here\n");
+	if (args.n_of_rounds != -1)
+		printf("n_of_rounds: %d\n", args.n_of_rounds);
 	return (0);
 }
