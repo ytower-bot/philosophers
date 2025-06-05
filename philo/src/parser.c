ae1dd98e8f09dfc536aba89ab7b97e33ffd92e74
yfaustin <yfaustin@student.42.rio>
2025-06-05 20:51:18 -0300

    todo

---
+++ philo/src/parser.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yfaustin <yfaustin@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/05 20:37:37 by yfaustin          #+#    #+#             */
+/*   Updated: 2025/06/05 20:40:31 by yfaustin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philosophers.h"
+
+int	parse_args(t_args *args, int argc, char **argv)
+{
+	// temq fazer
+	(void)argc;
+	(void)argv;
+	(void)args;
+	return (1);
+}
