2df514903f0f01c267053ee9a029c1ebcf3816a0
yfaustin <yfaustin@student.42.rio>
2025-06-05 20:37:01 -0300

    refactor

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,7 +6,7 @@
 /*   By: yfaustin <yfaustin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 14:06:01 by yfaustin          #+#    #+#             */
-/*   Updated: 2025/06/03 19:06:08 by yfaustin         ###   ########.fr       */
+/*   Updated: 2025/06/05 20:32:56 by yfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,18 @@
 
 int	main(int argc, char **argv)
 {
-	t_args	args;
+	t_table	table;
 
-	if (!(check_args(&args, argc, argv)))
+	if (!(check_args(&table.args, argc, argv)))
 		return (1);
-	printf("number of philosophers: %d\n", args.n_of_philo);
-	printf("time to die: %lld\n", args.time_to_die);
-	printf("time to eat: %lld\n", args.time_to_eat);
-	printf("time to sleep: %lld\n", args.time_to_sleep);
-	if (args.n_of_rounds != -1)
-		printf("number of rounds: %d\n", args.n_of_rounds);
+	printf("number of philosophers: %d\n", table.args.n_of_philo);
+	printf("time to die: %lld\n", table.args.time_to_die);
+	printf("time to eat: %lld\n", table.args.time_to_eat);
+	printf("time to sleep: %lld\n", table.args.time_to_sleep);
+	if (table.args.n_of_rounds != -1)
+		printf("number of rounds: %d\n", table.args.n_of_rounds);
+	if (!init(&table, argc, argv))
+		return (1);
+	printf("Simulation started\n");
 	return (0);
 }
