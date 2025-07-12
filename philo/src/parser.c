4b16ed329ff7ebc8e9830ffbd82d9cd5404ede3f
ytower <ytower@proton.me>
2025-07-12 17:37:21 -0300

    fix variable name

--- philo/src/parser.c
+++ philo/src/parser.c
@@ -6,7 +6,7 @@
 /*   By: yfaustin <yfaustin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/02 19:33:58 by yfaustin          #+#    #+#             */
-/*   Updated: 2025/06/30 20:05:21 by yfaustin         ###   ########.fr       */
+/*   Updated: 2025/07/12 17:37:10 by yfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 
 static void	init_args(t_args *args)
 {
-	args->n_of_philo = 0;
+	args->n_philo = 0;
 	args->time_to_die = 0;
 	args->time_to_eat = 0;
 	args->time_to_sleep = 0;
-	args->n_of_rounds = -1;
+	args->rounds = -1;
 	args->valid_args = 0;
 }
 
@@ -28,7 +28,7 @@ int	parse_args(t_args *args, int argc, char **argv)
 	init_args(args);
 	if (argc != 5 && argc != 6)
 		return (print_error("Invalid number of arguments"));
-	if (!ft_atopi(argv[1], &args->n_of_philo) || args->n_of_philo == 0)
+	if (!ft_atopi(argv[1], &args->n_philo) || args->n_philo == 0)
 		return (print_error("Invalid n_of_philo"));
 	if (!ft_atopll(argv[2], &args->time_to_die))
 		return (print_error("Invalid <time_to_die>"));
@@ -42,9 +42,9 @@ int	parse_args(t_args *args, int argc, char **argv)
 		return (print_error("Time values too large"));
 	if (argc == 6)
 	{
-		if (!ft_atopi(argv[5], &args->n_of_rounds))
+		if (!ft_atopi(argv[5], &args->rounds))
 			return (print_error("Invalid <n_of_rounds>"));
-		if (args->n_of_rounds <= 0)
+		if (args->rounds <= 0)
 			return (print_error("Number of rounds must be positive"));
 	}
 	args->valid_args = 1;
