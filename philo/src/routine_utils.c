4dcf1bf152d1ff62d498c123b994408d06f1b83e
ytower <ytower@proton.me>
2025-07-12 18:39:28 -0300

    norminette

--- philo/src/routine_utils.c
+++ philo/src/routine_utils.c
@@ -6,12 +6,22 @@
 /*   By: yfaustin <yfaustin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/09 19:37:28 by yfaustin          #+#    #+#             */
-/*   Updated: 2025/07/12 17:50:03 by yfaustin         ###   ########.fr       */
+/*   Updated: 2025/07/12 18:39:15 by yfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosophers.h"
 #include "../includes/utils.h"
+
+static void	eat(t_philosopher *philo)
+{
+	pthread_mutex_lock(&philo->table->meal_mutex);
+	philo->last_meal_ts = get_time();
+	philo->meal_count++;
+	pthread_mutex_unlock(&philo->table->meal_mutex);
+	print_status(philo, "is eating");
+	precise_sleep(philo->table->args.t_eat, philo->table);
+}
 
 void	philo_eat(t_philosopher *philo)
 {
@@ -29,12 +39,7 @@ void	philo_eat(t_philosopher *philo)
 		pthread_mutex_lock(philo->lfork);
 		print_status(philo, "has taken a fork");
 	}
-	pthread_mutex_lock(&philo->table->meal_mutex);
-	philo->last_meal_ts = get_time();
-	philo->meal_count++;
-	pthread_mutex_unlock(&philo->table->meal_mutex);
-	print_status(philo, "is eating");
-	precise_sleep(philo->table->args.t_eat, philo->table);
+	eat(philo);
 	if (philo->id % 2 == 0)
 	{
 		pthread_mutex_unlock(philo->rfork);
