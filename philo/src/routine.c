6fce3d44fb630fa3b612672ad980cd5ace606f22
ytower <ytower@proton.me>
2025-07-12 18:35:11 -0300

    norminette

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,7 +6,7 @@
 /*   By: yfaustin <yfaustin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 19:46:12 by yfaustin          #+#    #+#             */
-/*   Updated: 2025/07/12 17:49:24 by yfaustin         ###   ########.fr       */
+/*   Updated: 2025/07/12 18:31:06 by yfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,11 +41,13 @@ void	monitor(t_table *table)
 int	start_simulation(t_table *table)
 {
 	int	i;
+	int	va;
 
 	i = 0;
 	while (i < table->args.n_philo)
 	{
-		if (pthread_create(&table->philo[i].thread, NULL, &routine, &table->philo[i]) != 0)
+		va = pthread_create(&table->philo[i].tid, NULL, &act, &table->philo[i]);
+		if (va != 0)
 			return (print_error("Failed creating philosopher thread"));
 		i++;
 	}
@@ -53,26 +55,34 @@ int	start_simulation(t_table *table)
 	i = 0;
 	while (i < table->args.n_philo)
 	{
-		if (pthread_join(table->philo[i].thread, NULL) != 0)
+		if (pthread_join(table->philo[i].tid, NULL) != 0)
 			return (print_error("Failed joining philosopher thread"));
 		i++;
 	}
 	return (1);
 }
 
-void	*routine(void *arg)
+static void	*lonely_philo(t_table *table)
+{
+	t_philosopher	*philo;
+
+	philo = table->philo;
+	if (table->args.n_philo == 1)
+	{
+		print_status(philo, "has taken a fork");
+		precise_sleep(table->args.t_die, table);
+	}
+	return (NULL);
+}
+
+void	*act(void *arg)
 {
 	t_philosopher	*philo;
 	t_table			*table;
 
 	philo = (t_philosopher *)arg;
 	table = philo->table;
-	if (table->args.n_philo == 1)
-	{
-		print_status(philo, "has taken a fork");
-		precise_sleep(table->args.t_die, table);
-		return (NULL);
-	}
+	lonely_philo(table);
 	if (philo->id % 2 == 0)
 		precise_sleep(5, philo->table);
 	while (1)
