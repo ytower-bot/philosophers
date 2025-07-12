5a8f9009ae0a958f9f66b8a7e0f54cd9e2b3a3cb
ytower <ytower@proton.me>
2025-07-12 17:35:43 -0300

    norminette

--- philo/src/monitor_utils.c
+++ philo/src/monitor_utils.c
@@ -6,7 +6,7 @@
 /*   By: yfaustin <yfaustin@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 20:39:01 by yfaustin          #+#    #+#             */
-/*   Updated: 2025/06/30 21:07:53 by yfaustin         ###   ########.fr       */
+/*   Updated: 2025/07/12 17:19:22 by yfaustin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	meal_check(t_table *table, int *all_have_eaten)
 
 	*all_have_eaten = 1;
 	i = -1;
-	while (++i < table->args.n_of_philo)
+	while (++i < table->args.n_philo)
 	{
 		pthread_mutex_lock(&table->meal_mutex);
 		if (get_time() - table->philo[i].last_meal_ts > table->args.time_to_die)
@@ -31,7 +31,8 @@ void	meal_check(t_table *table, int *all_have_eaten)
 			pthread_mutex_unlock(&table->meal_mutex);
 			return ;
 		}
-		if (table->args.n_of_rounds != -1 && table->philo[i].meal_count < table->args.n_of_rounds)
+		if (table->args.rounds != -1)
+			if (table->philo[i].meal_count < table->args.rounds)
 			*all_have_eaten = 0;
 		pthread_mutex_unlock(&table->meal_mutex);
 	}
